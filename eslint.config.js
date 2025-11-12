1f05f17caef02a1bf854e5391d878f21f580e9ce
jessekrubin <jessekrubin@gmail.com>
2025-11-12 08:30:40 -0800

    update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,7 @@
 import jsse from "@jsse/eslint-config";
 
-export default jsse(
-  {
-    debug: false,
-    // typescript: {
-    //   tsconfig: ["tsconfig.json", "tsconfig.eslint.json"],
-    // },
-    reportUnusedDisableDirectives: true,
-    // off: ["dot-notation"],
-    prettier: true,
-    // gitignore: {
-    //   files: [".gitignore"],
-    //   strict: true,
-    //   root: true
-    // }
-  },
-  /**
-   * overrides
-   */
-  // ...[
-  //   {
-  //     files: ["*.ts", "*.tsx"],
-  //     rules: {}
-  //   }
-  // ]
-);
+export default jsse({
+  debug: false,
+  reportUnusedDisableDirectives: true,
+  prettier: true,
+});
